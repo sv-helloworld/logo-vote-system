fcd2403c34b8aff514650d854bffa3f2572af4f3
Thijs Clowting <thijs@clowting.me>
2016-04-08 21:58:17 +0200

    E-mail field, style changes, show result after voting only.

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,13 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.endSymbol('}]}');
 }]);
 
-app.controller('VoteController', ['$http', '$scope', 'ChartJs', function($http, $scope, ChartJs) {
+app.controller('VoteController', ['$http', '$scope', '$timeout', 'ChartJs', function($http, $scope, $timeout, ChartJs) {
 
     $scope.total = 0;
     $scope.results = {};
     $scope.chartLabels = [];
     $scope.chartData = [];
+    $scope.state = 'vote';
 
     // Load current statistics
     $http.get('/vote/statistics').then(function(result) {
@@ -26,12 +27,20 @@ app.controller('VoteController', ['$http', '$scope', 'ChartJs', function($http, 
 
     // Vote function
     $scope.vote = function(id) {
-        $http.get('/vote/' + id).then(function(result) {
+        $http.get('/vote/' + id + ($scope.email !== '' ? '/' + $scope.email : '')).then(function(result) {
+
+            $scope.state = 'statistics';
 
             $scope.total = result.data.total;
             $scope.results = result.data.results;
             $scope.chartLabels = result.data.chart.labels;
             $scope.chartData = result.data.chart.data;
+
+            $scope.email = '';
+
+            $timeout(function() {
+                $scope.state = 'vote';
+            }, 8000);
 
         }, function(error) {
 
